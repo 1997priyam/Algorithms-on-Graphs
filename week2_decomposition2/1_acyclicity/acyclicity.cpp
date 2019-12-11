19970af016e333841dd8c76a38540dd6800b2e7f
1997priyam <1997priyam@gmail.com>
2019-12-11 12:29:03 +0530

    Partial soluton for detecting cycle in directed graph

--- week2_decomposition2/1_acyclicity/acyclicity.cpp
+++ week2_decomposition2/1_acyclicity/acyclicity.cpp
@@ -4,9 +4,24 @@
 using std::vector;
 using std::pair;
 
+void explore(vector<vector<int> > adj, int vertex, vector<int> &visited, int &found){
+  visited[vertex] = 1;
+  for(auto neighbour: adj[vertex]){
+    if(visited[neighbour] == 0 && found == 0)
+    explore(adj, neighbour, visited, found);
+    else found = 1;
+  }  
+}
+
 int acyclic(vector<vector<int> > &adj) {
   //write your code here
-  return 0;
+  vector<int> visited(adj.size(), 0);
+  int found = 0;
+  for(int i=0; i<adj.size(); i++){
+    if(visited[i] == 0 && found == 0)
+    explore(adj, i, visited, found);
+  }
+  return found;
 }
 
 int main() {
