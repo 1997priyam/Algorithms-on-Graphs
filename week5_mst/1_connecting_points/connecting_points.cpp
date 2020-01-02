6545f293e584add2d64a72e9279e6ba79eddb593
1997priyam <1997priyam@gmail.com>
2020-01-02 23:20:59 +0530

    Building road network using kruskal's half implemented

--- week5_mst/1_connecting_points/connecting_points.cpp
+++ week5_mst/1_connecting_points/connecting_points.cpp
@@ -6,6 +6,41 @@
 
 using std::vector;
 
+class node{
+  public:
+    int x;
+    int y;
+    int parent;
+    int rank;
+
+    node(int x, int y, int parent = -1, int rank = 0){
+      this->x = x;
+      this->y = y;
+      this->parent = parent;
+      this->rank = rank;
+    }
+};
+
+class edge{
+  public:
+    int u;
+    int v;
+    double weight;
+
+    edge(int u, int v, double weight){
+      this->u = u;
+      this->v = v;
+      this->weight = weight;
+    }
+
+};
+
+void make_set(vector<node> &nodes, vector<int> &x, vector<int> &y){
+  for(int i=0; i<x.size(); i++){
+    nodes.push_back(node(x[i], y[i], i));
+  }
+}
+
 double minimum_distance(vector<int> x, vector<int> y) {
   double result = 0.;
   //write your code here
