7c5ef444c607edc9aeabe5644f6ceaa28e41dee2
1997priyam <1997priyam@gmail.com>
2020-01-03 18:17:41 +0530

    Completed the connecting roads problem using Kruskal's algo

--- week5_mst/1_connecting_points/connecting_points.cpp
+++ week5_mst/1_connecting_points/connecting_points.cpp
@@ -35,6 +35,10 @@ class edge{
 
 };
 
+double get_weight(int x1, int y1, int x2, int y2) {
+  return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
 void make_set(vector<node> &nodes, vector<int> &x, vector<int> &y){
   for(int i=0; i<x.size(); i++){
     nodes.push_back(node(x[i], y[i], i));
@@ -49,12 +53,31 @@ void make_edges(vector<edge> &edges, vector<int> &x, vector<int> &y){
 	}
 }
 
-double get_weight(int x1, int y1, int x2, int y2) {
-  return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-}
-
 bool cmp(edge a, edge b) {
   return a.weight < b.weight;
+}
+
+int find(int i, vector<node> &nodes) {
+  if (i != nodes[i].parent) {						// If the parent of the node is not equal to itself then keep going recursively
+    nodes[i].parent = find(nodes[i].parent, nodes);	// Change the parent to directly the parent node so that intermediate steps are eliminated
+  }
+  return nodes[i].parent;	
+}
+
+void Union(int u, int v, vector<node> &nodes) {		// Merging the two nodes in 1 set
+  int r1 = find(u, nodes);
+  int r2 = find(v, nodes);
+  if (r1 != r2) {	
+    if (nodes[r1].rank > nodes[r2].rank) {			// Checking the rank of parent of each node and attaching the node
+      nodes[r2].parent = r1;						// under the one with greater rank
+    } 
+	else {
+      nodes[r1].parent = r2;
+      if (nodes[r1].rank == nodes[r2].rank) {		// If the rank of boith is equal then attach under any node and increase its rank
+        nodes[r2].rank++;
+      }
+    }
+  }
 }
 
 double minimum_distance(vector<int> x, vector<int> y) {
@@ -62,9 +85,16 @@ double minimum_distance(vector<int> x, vector<int> y) {
   //write your code here
   vector<node> nodes;
   vector<edge> edges;
-  make_set(nodes, x, y);
-  make_edges(edges, x, y);
-  std::sort(edges.begin(), edges.end(), cmp);
+  make_set(nodes, x, y);		// Creating the Disjoint set
+  make_edges(edges, x, y);		// Creating the edges of unidirected graph
+  std::sort(edges.begin(), edges.end(), cmp);	// Sorting the edges in increasing order
+
+  for(edge edge: edges){		// Iterating over each edge for Kruskal's algorithm
+	  if(find(edge.u, nodes) != find(edge.v, nodes)){	// If two nodes are not in same set 
+		  result += edge.weight;						// Add the weight of the edge to the result
+		  Union(edge.u, edge.v, nodes);					// Merge the two nodes so that they are in same set now.
+	  }
+  }
   return result;
 } 
 
